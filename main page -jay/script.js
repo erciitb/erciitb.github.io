f8ec9a6f66e4131456160141a34fae3bb2ed9c70
Shubham Ojha <63144588+CommanderBaman@users.noreply.github.com>
2020-09-18 20:27:49 +0530

    added basic scrollbar; some images compressed;

--- main page -jay/script.js
+++ main page -jay/script.js
@@ -51,6 +51,22 @@ for animating the navbar
 
 })(jQuery);
 
+
+
+/*
+==========================================
+After this part is added by Shubham Ojha
+==========================================
+*/
+// some global variables
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+window.addEventListener( "resize", function() {
+	windowWidth = window.innerWidth;
+	windowHeight = window.innerHeight;
+})
+
 /*
 ==========================================
 for animating the logo
@@ -69,88 +85,109 @@ VanillaTilt.init( ercLogo, {
 });
 
 
-// // for fixing the width of the erc-container - scrollbar coming in
-const ercLogoContainer = document.getElementById("erc-logo-container")
-logoWidth = ercLogo.clientWidth
-logoHeight = ercLogo.clientHeight
-ercLogoContainer.style.width = logoWidth + "px";
-ercLogoContainer.style.height = logoHeight + "px";
-console.log( ercLogoContainer.style.width, ercLogoContainer.style.height)
-
-
 /*
 ==========================================
-For animating scroll bot
+For animating scroll bot = this is only available for firefox till now
 ==========================================
 */
-const scrollBot = document.querySelector("#scroll-bot img");
-const scrollBotContainer = document.querySelector("#scroll-bot");
-let windowWidth = window.innerWidth;
-let windowHeight = window.innerHeight;
-let scrollBotOffset = -50;
 
-const body = document.body;
-const html = document.documentElement;
-const fullBodyHeight = Math.max( body.scrollHeight, body.offsetHeight, html.scrollHeight, html.offsetHeight );
-
-// marking end of the body
-const ender = document.getElementsByClassName("ender")[0];
-let enderPos = ender.getBoundingClientRect()['top'];
-let usingEnder = false;
-let scrollEndPosition = 0;
-
-if ( 0.9 * fullBodyHeight <= enderPos <= 1.1 * fullBodyHeight ) {
-	console.log('can do by enderPos');
-	usingEnder = true;
-	scrollEndPosition = enderPos - windowHeight;
-}
-
-let pxScrolled = 0;
-let percentScrolled = 0;
-// window.scroll = adjustScollImage(); 
-window.addEventListener( "scroll", adjustScollImage);
-
-function adjustScollImage() {
-	let scrollYPos = this.scrollY;
-	// getMousePosition(window.event);
-
-	if (usingEnder) {
-		percentScrolled = scrollYPos / scrollEndPosition * 100;
-		scrollBot.style.top = percentScrolled + "%";
-	} 
-	else {
-		console.log('scroll is broken');
-		pxScrolled = scrollYPos / fullBodyHeight * windowHeight;
-		scrollBot.style.top = pxScrolled + "px";
-		// let percentScrolled = pxScrolled * 100;
-	}
-}
-
-
-// on holding the mouse button, the page should scroll
-var timeoutId = 0;
-
-$('#scroll-bot img').on('mousedown', function(e) {
-    let timeoutId = setInterval(getMousePosition(e), 1);
-}).on('mouseup mouseleave', function() {
-    clearTimeout(timeoutId);
-});
-
-function getMousePosition() {
-	let e = window.event;
-
-	console.log("Mouse position: ", e.clientX, e.clientY);
-}
-
-// bringing back scroll bot for firefox - next 4 lines are check
+// handled with scrollbar for others - bringing back scroll bot for firefox - next 4 lines are check
 // var userAg = navigator.userAgent;
 // console.log(userAg);
 // console.log( userAg.indexOf("Chrome"), userAg.indexOf("Firefox"));
 // console.log( userAg.indexOf("Opera"), userAg.indexOf("MSIE"), userAg.indexOf("Safari"));
 
-if ( navigator.userAgent.indexOf("Firefox") != -1) {
+if ( navigator.userAgent.indexOf("Firefox") != -1 && windowWidth > 768) {
 	console.log("Firefox detected");
+	const scrollBot = document.querySelector("#scroll-bot img");
+	const scrollBotContainer = document.querySelector("#scroll-bot");
+	
+	const body = document.body;
+	const html = document.documentElement;
+	const fullBodyHeight = Math.max( body.scrollHeight, body.offsetHeight, html.scrollHeight, html.offsetHeight );
+	
+	// marking end of the body
+	const ender = document.getElementsByClassName("ender")[0];
+	let enderPos = ender.getBoundingClientRect()['top'];
+	let usingEnder = false;
+	let scrollEndPosition = 0;
+	
+	if ( 0.9 * fullBodyHeight <= enderPos <= 1.1 * fullBodyHeight ) {
+		// console.log('can do by enderPos');
+		usingEnder = true;
+		scrollEndPosition = enderPos - windowHeight;
+	}
+	
+	let pxScrolled = 0;
+	let percentScrolled = 0;
+	// window.scroll = adjustScollImage(); 
+	window.addEventListener( "scroll", adjustScollImage);
+	
+	function adjustScollImage() {
+		let scrollYPos = this.scrollY;
+		// getMousePosition(window.event);
+	
+		if (usingEnder) {
+			percentScrolled = scrollYPos / scrollEndPosition * 100;
+			scrollBot.style.top = percentScrolled + "%";
+		} 
+		else {
+			alert("Scroll is broken! Please refresh the page! continue with the browsing if the error persists")
+			pxScrolled = scrollYPos / fullBodyHeight * windowHeight;
+			scrollBot.style.top = pxScrolled + "px";
+			// let percentScrolled = pxScrolled * 100;
+		}
+	}
+	
+	// this means that the person is scrolling
+	let scrollActivated = false;
+	
+	// adding event listeners 
+	scrollBotContainer.addEventListener("mousedown", pressingDown, false);
+	scrollBotContainer.addEventListener("mouseup", notPressingDown, false);
+	scrollBotContainer.addEventListener("mousemove", handleScrollBot, false);
+	window.addEventListener("mouseup", notPressingDown, false);
+	
+	function pressingDown(e) {
+		scrollActivated = true;
+		e.preventDefault();
+		handleScrollBot(e);
+	}
+	
+	function notPressingDown(e) {
+		scrollActivated = false;
+		e.preventDefault();
+	}
+	
+	function handleScrollBot(e) {
+		if (scrollActivated) {
+			// getting y position of mouse
+			let mouseY = e.clientY;
+			// console.log("scrollBot pressed!", mouseY)
+			
+			let percentScrolled = mouseY/windowHeight * 100;
+			window.scrollTo( 0, percentScrolled * scrollEndPosition / 100);
+			// scrollBot.style.top = percentScrolled + "%";
+			scrollBot.style.top = mouseY + "px";
+			
+			// console.log("Scrolling to: ", percentScrolled * scrollEndPosition / 100, " scroll bot at: ", mouseY, "scroll height is: ", scrollEndPosition);
+		}
+		else {
+			// console.log("scroll not activated!");
+		}
+	}
 	scrollBotContainer.style.display = "block";
+
+	// vanishing and reappearing on window resize
+	window.addEventListener( "resize", function() {
+		if ( windowWidth < 768) {
+			// console.log("run");
+			scrollBotContainer.style.display = "none";
+		}
+		else {
+			scrollBotContainer.style.display = "block";
+		}
+	})
 }
 
 
